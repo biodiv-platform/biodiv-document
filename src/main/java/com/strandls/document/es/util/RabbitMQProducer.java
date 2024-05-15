5f82334e6340343e2733d28ec0464d7e8b7008bf
Arunbala Balasubramani <arunbala.balasubramani@strandls.com>
2024-05-15 10:13:48 +0530

    fix: :bug: rabbitmq exchange bug fix

--- src/main/java/com/strandls/document/es/util/RabbitMQProducer.java
+++ src/main/java/com/strandls/document/es/util/RabbitMQProducer.java
@@ -9,6 +9,7 @@ import javax.inject.Inject;
 
 import com.rabbitmq.client.AMQP.BasicProperties;
 import com.rabbitmq.client.Channel;
+import com.strandls.document.util.PropertyFileUtil;
 
 /**
  * 
@@ -17,7 +18,7 @@ import com.rabbitmq.client.Channel;
  */
 public class RabbitMQProducer {
 
-	private static final String EXCHANGE_BIODIV = "biodiv";
+	private String EXCHANGE = PropertyFileUtil.fetchProperty("config.properties", "rabbitmq_exchange");
 
 	@Inject
 	private Channel channel;
@@ -25,11 +26,11 @@ public class RabbitMQProducer {
 	public void setMessage(final String routingKey, String message, String documentId) throws Exception {
 
 		try {
-		BasicProperties properties = new BasicProperties(null, null, null, 2, 1, null, null, null, null, null,
-				documentId, null, null, null);
-		channel.basicPublish(EXCHANGE_BIODIV, routingKey, properties, message.getBytes("UTF-8"));
-		System.out.println(" [RABBITMQ] Sent Document Id: '" + message + "'");
-		}catch(IOException e) {
+			BasicProperties properties = new BasicProperties(null, null, null, 2, 1, null, null, null, null, null,
+					documentId, null, null, null);
+			channel.basicPublish(EXCHANGE, routingKey, properties, message.getBytes("UTF-8"));
+			System.out.println(" [RABBITMQ] Sent Document Id: '" + message + "'");
+		} catch (IOException e) {
 			System.out.print("==================================");
 			System.out.print(e.toString());
 			System.out.print("==================================");
